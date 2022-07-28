6dc02741ccc59681f0341956118ff865c223f17a
Harry <L3478830@163.com>
2022-07-28 17:02:30 +0800

    feat: change test file name

--- png2j_test.go
+++ png2j_test.go
@@ -1,16 +1,19 @@
-package png2j
+package png2j_test
 
-import "testing"
+import (
+	"github.com/ser163/png2j"
+	"testing"
+)
 
 func TestCon2jpg(t *testing.T) {
-	err := Con2jpg("./image/go.png", "./image/go.png.jpg")
+	err := png2j.Con2jpg("./image/go.png", "./image/go.png.jpg")
 	if err != nil {
 		t.Errorf("%v", err)
 	}
 }
 
 func TestReSizeImage(t *testing.T) {
-	err := ReSizeImage("./image/go.png.jpg", 948, 418, "./image/go_big.png.jpg")
+	err := png2j.ReSizeImage("./image/go.png.jpg", 948, 418, "./image/go_big.png.jpg")
 	if err != nil {
 		t.Errorf("%v", err)
 	}
